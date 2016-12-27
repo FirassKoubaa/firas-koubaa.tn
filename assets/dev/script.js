358e55266a71713762b2cd4dc3ed80fa50d0a601
Firas KOUBÀA <firas.koubaa.stg@sifast.com>
2016-12-27 01:32:43 +0100

    DEV of counting page visting number and cron of localstorage at midnight

--- assets/dev/script.js
+++ assets/dev/script.js
@@ -14,28 +14,33 @@ $(function () {
         $.ajax({
             type: 'POST',
             url: 'assets/dev/email.php',
-            data:msgtext ,
+            data: msgtext,
             success: function () {
                 //alert('form was submitted');
             },
-            error: function() {
+            error: function () {
                 alert('EMAIL NOT SENT ');
             }
         });
     });
 
 
+// PAGE COUNTER
 
+    // CRON CLEARING LOCAL STORAGE
+    var schedule = require('node-schedule'),
+        cron = schedule.scheduleJob({hour: 00, minute: 00}, function () {
+            localStorage.clear();
+        });
 
-
-
-
-
-
-
-
-
-
+    // COUNTING
+    if (localStorage.pagecount) {
+        localStorage.pagecount = Number(localStorage.pagecount) + 1;
+    }
+    else {
+        localStorage.pagecount = 1;
+    }
+    console.log("Number of visits until now is: " + localStorage.pagecount + " time(s).");
 
 
 });
